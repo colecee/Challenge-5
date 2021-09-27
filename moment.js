b8dcf12ac066f026d0796a4cfcaa54d1c0908eb2
colecee <88739613+colecee@users.noreply.github.com>
2021-09-26 20:17:55 -0500

    Add files via upload

---
+++ moment.js
@@ -0,0 +1,5 @@
+moment().format('MMMM Do YYYY, h:mm:ss a');
+moment().format('dddd');                    
+moment().format("MMM Do YY");              
+moment().format('YYYY [escaped] YYYY');
+moment().format(); 
